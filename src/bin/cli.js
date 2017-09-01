4773e674a0dd254e6af1ae1c4cb6c80764dc9e57
Jason Haruska <jason@haruska.com>
2017-09-01 16:11:31 -0500

    add start of cli and a hello message

---
+++ src/bin/cli.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+import minimist from 'minimist'
+import shell from 'shelljs'
+import path from 'path'
+import commands from '../lib/commands'
+
+const main = () => {
+  const args = minimist(process.argv.slice(2), {
+    alias: { h: 'help', v: 'version' },
+    string: ['source', 'name', 'region', 'profile'],
+    boolean: ['quiet'],
+    default: { 'source': shell.pwd().toString() }
+  })
+
+  const command = args._ && args._.length && args._[0]
+
+  if (args.version && !command) {
+    console.log(require(path.join(__dirname, '..', 'package.json')).version)
+    return
+  }
+
+  if (command && !commands[command]) {
+    console.error(`unsupported command ${command}. re-run with --help for usage information`);
+    process.exit(1);
+    return;
+  }
+
+  // if (args.help) {
+  //   if (command) {
+  //     console.log(docTxt.commandDoc(commands[command]));
+  //   } else {
+  //     console.log(docTxt.index(commands));
+  //   }
+  //   return;
+  // }
+
+  if (!command) {
+    console.error('command not provided. re-run with --help for usage information');
+    process.exit(1);
+    return;
+  }
+
+  commands[command](args)
+  process.exit();
+}
+
+main();
---
+++ src/lib/commands/hello.js
@@ -0,0 +1,5 @@
+import shell from 'shelljs'
+
+export default function(options) {
+  shell.echo("Hello from Thegas!")
+}
