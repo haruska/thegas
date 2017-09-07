50e51f65bd8caefccd77f249b3bab3e239bdb521
Jason Haruska <jason@haruska.com>
2017-09-07 15:58:06 -0500

    add help text to cli

--- src/bin/cli.js
+++ src/bin/cli.js
@@ -25,14 +25,29 @@ const main = () => {
     return
   }
 
-  // if (args.help) {
-  //   if (command) {
-  //     console.log(docTxt.commandDoc(commands[command]));
-  //   } else {
-  //     console.log(docTxt.index(commands));
-  //   }
-  //   return;
-  // }
+  if (args.help) {
+    const doc = `
+usage: thegas [command] {OPTIONS}
+
+Deploy a GraphQL endpoint to AWS as a lambda microservice.
+
+COMMANDS are:
+  schema        regenerates schema definition
+  server        runs a local server graph endpoint
+  build         builds project to a temp directory
+  deploy  [-p]  deploys to aws and returns an endpoint
+                  -p : production (default staging)
+
+OPTIONS are:
+
+  --help        print this help screen
+  --version     print out the current version
+
+`
+    console.log(doc)
+    process.exit(0)
+    return
+  }
 
   if (!command) {
     console.error('command not provided. re-run with --help for usage information')
